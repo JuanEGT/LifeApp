632cc626c40d59b13d7052a5b6bdb0f7f41f2897
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-09-10 22:48:04 -0600

    agregar archivos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 👇 pon aquí tu WebApp URL de Google Apps Script
-const API_URL = "https://script.google.com/macros/s/AKfycbxZlkiUlVRWpWJseCJSbUH_EwywzeHkDNZsTqFQ5M-1l5_My9VX4LiCZ344WPywXudKng/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzKwLrAc3oRBVO54y1o3KWXKM_xD-ZCdoOrTyz6S8vJpHBEJy0rDSQVVioFHY-ZnzIS5Q/exec";
 const SHEET = "Agenda";
 
 // ===============================
