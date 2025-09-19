6dc64454fa293e44a8393152165a2363f97e33b0
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-09-19 14:02:47 -0600

    agregar archivos

--- js/finanzas.js
+++ js/finanzas.js
@@ -7,7 +7,6 @@ const SPREADSHEET_ID = "1CMnA-3Ch5Ac1LLP8Hgph15IeeH7Dlvcj0IvX51mLzKU";
 const SHEET_NAME = "Finanzas";
 let finanzasData = [];
 let charts = {};
-let token = null; // token para Google Sheets
 
 // ------------------------
 // 0️⃣ Token
