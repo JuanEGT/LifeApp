35ea491b2a0d15981ed2eee0654283ecd893f545
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-09-19 13:17:32 -0600

    agregar archivos

--- js/tar_hab.js
+++ js/tar_hab.js
@@ -1,17 +1,18 @@
 // ===================== tar_hab.js =====================
 const TarHab = (function () {
-
   // Mostrar el módulo
   function mostrarTarHab() {
     document.getElementById("tarHabContainer").style.display = "flex";
     document.getElementById("mainMenu").style.display = "none";
   }
 
-  // Inicializar botones
-  document.querySelectorAll(".btnVolverTarHab")?.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      document.getElementById("tarHabContainer").style.display = "none";
-      document.getElementById("mainMenu").style.display = "flex";
+  // Inicializar botones solo cuando DOM esté listo
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".btnVolverTarHab")?.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        document.getElementById("tarHabContainer").style.display = "none";
+        document.getElementById("mainMenu").style.display = "flex";
+      });
     });
   });
 
