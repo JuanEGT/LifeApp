c1a19f8dda31f9bc6c515768f934a3686839b4ed
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-09-19 15:44:58 -0600

    agregar archivos

---
+++ js/habitos.js
@@ -0,0 +1,26 @@
+// ===================== NOMBRE DEL Habitos.JS =====================
+let HabitosToken = null; // Token específico de este Habitos
+
+function setToken(token) {
+  HabitosToken = token;
+}
+
+// Función de inicialización del Habitos
+function initHabitos() {
+  console.log("[Habitos] Inicializando Habitos");
+
+  const backBtn = document.getElementById("backToHomeBtn");
+  if (backBtn) {
+    console.log("[Habitos] Botón de volver al Home encontrado");
+    backBtn.addEventListener("click", () => {
+      console.log("[Habitos] Volviendo al Home");
+      volverHome(); // Función global en main.js
+    });
+  } else {
+    console.warn("[Habitos] Botón de volver al Home NO encontrado");
+  }
+}
+
+// Exponer funciones al scope global
+window.setToken = setToken;
+window.initHabitos = initHabitos;
