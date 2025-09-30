57e5d66fda431c880e21dd0730c913a3656e98b5
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-09-29 23:41:11 -0600

    agregar archivos

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
     const CLIENT_ID = "721915958995-9gri9jissf6vp3i1sfj93ft3tjqp7rnk.apps.googleusercontent.com";
     const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
     const SPREADSHEET_ID = "1CMnA-3Ch5Ac1LLP8Hgph15IeeH7Dlvcj0IvX51mLzKU";
-    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwTF_4IhuQzO3C1pvvNHTusdSWvBjNyRycdLF3AppAxZ9cqHLoZWeldBbaeFS88aHGbXA/exec";
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxnUXIPZJA1QZAk8YQA7AdCKjinMyLbnSgvKOr4kZZGgpKHqmVozitlA0-H4883VeBjrg/exec";
 
     // Variables globales compartidas
     var token = null;
