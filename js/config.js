344cc9bc1fabeaccd11ca8c0a849ed15b013dd91
JuanEGT <juaneliasgutierreztorres@gmail.com>
2025-10-13 00:13:10 -0600

    cambio

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
     const CLIENT_ID = "721915958995-9gri9jissf6vp3i1sfj93ft3tjqp7rnk.apps.googleusercontent.com";
     const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
     const SPREADSHEET_ID = "1CMnA-3Ch5Ac1LLP8Hgph15IeeH7Dlvcj0IvX51mLzKU";
-    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzWozjdQ3n-bHTPF4yOMaLKrrl6i6OlKVk4VqjWIht1_n8l3euyQNHfNJIDPvo46tCCAw/exec";
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxIXRW1xeL_-Q2GTCt1dwu0JvnwtQMUt0Xb3lpnbs1GdT04GFpI4X4_NxKQIxd3bQwt_g/exec";
 
     // Variables globales compartidas
     var token = null;
